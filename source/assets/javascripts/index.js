0d374e466a6a1832d71dd404907a1d05ef1dd4a6
Piotr Polanski <polanski.pio@gmail.com>
2021-09-13 00:08:22 +0900

    added img

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -3,7 +3,7 @@ import 'bootstrap';
 function init() {
   const style = ['style1', 'style2', 'style3', 'style4'];
   const size = ['size1', 'size1', 'size1', 'size2', 'size3'];
-  const opacity = ["opacity1", "opacity1", "opacity1", "opacity2", "opacity2", "opacity3"];
+  const opacity = ['opacity1', 'opacity1', 'opacity1', 'opacity2', 'opacity2', 'opacity3'];
 
   function getRandomArbitrary(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
@@ -15,6 +15,7 @@ function init() {
   const widthWindow = window.innerWidth;
   const heightWindow = window.innerHeight;
 
+  // eslint-disable-next-line no-plusplus
   for (let i = 0; i < starsNumber; i++) {
     star += `<span class='star ${style[getRandomArbitrary(0, 4)]} ${opacity[getRandomArbitrary(0, 6)]} ${size[getRandomArbitrary(0, 5)]}' style='animation-delay: .${getRandomArbitrary(0, 9)}s; left: ${getRandomArbitrary(0, widthWindow)}px; top: ${getRandomArbitrary(0, heightWindow)}px'></span>`;
   }
@@ -23,20 +24,18 @@ function init() {
 
   let randomNumber = 5000;
 
-  setTimeout(function () {
-    generateCommets();
-  }, randomNumber);
-
   const generateCommets = () => {
     setTimeout(generateCommets, randomNumber);
     randomNumber = getRandomArbitrary(5000, 10000);
-    const commet = "<div class='commet " + style[getRandomArbitrary(0, 4)] + "'></div>";
+    const commet = `<div class='commet ${style[getRandomArbitrary(0, 4)]}'></div>`;
     document.getElementsByClassName('commets-wrapper')[0].innerHTML = commet;
-    setTimeout(function () {
-      document.getElementsByClassName('commets-wrapper')[0].innerHTML = "";
+    setTimeout(() => {
+      document.getElementsByClassName('commets-wrapper')[0].innerHTML = '';
     }, 1000);
-  }
-
+  };
+  setTimeout(() => {
+    generateCommets();
+  }, randomNumber);
 }
 
 window.onload = init;
